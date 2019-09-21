57b883dcec910ab2dddb774b90607ae0c15ed58b
Rafael George <george.rafael@gmail.com>
2019-09-21 08:47:05 -0400

    Add unicode to test case

--- leetcode/ruby/valid_anagram/valid_anagram_spec.rb
+++ leetcode/ruby/valid_anagram/valid_anagram_spec.rb
@@ -8,7 +8,7 @@ end
 
 describe ValidAnagram do
   it "validates if two strings are anagrams" do
-    expect(ValidAnagram.call("anagram", "nagaram")).to be_truthy
-    expect(ValidAnagram.call("rat", "car")).to be_falsy
+    expect(ValidAnagram.call("ana©gram", "naga©ram")).to be_truthy
+    expect(ValidAnagram.call("rat©", "c©ar")).to be_falsy
   end
 end
