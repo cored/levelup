0e991be925f724648e38f1a0e4b3e9e61f51307d
Rafael George <george.rafael@gmail.com>
2021-03-16 11:59:12 -0600

    [TCR]

--- leetcode/array/max_consecutive_ones_spec.rb
+++ leetcode/array/max_consecutive_ones_spec.rb
@@ -1,6 +1,22 @@
 module MaxConsecutiveOnes
+  extend self
+
+  def call(nums)
+    0
+  end
 end
 
 RSpec.describe MaxConsecutiveOnes do
+  subject(:max_cons) { described_class}
+  let(:cases) do
+    {
+      [] => 0,
+    }
+  end
 
+  it 'find the maximum conosecutive ones in a binary array' do
+    cases.each_pair do |target, expectation|
+      expect(max_cons.(target)).to eql expectation
+    end
+  end
 end
